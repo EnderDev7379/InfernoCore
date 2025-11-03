d137d79f6ea4f58a07909ca3962e872c8bca2923
ender <enderdev7379@gmail.com>
2025-11-03 12:10:32 +0100

    Add Semi-Hardcore

--- src/main/java/net/gooseman/inferno_utils/InfernoUtils.java
+++ src/main/java/net/gooseman/inferno_utils/InfernoUtils.java
@@ -2,12 +2,19 @@ package net.gooseman.inferno_utils;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.fabric.api.entity.event.v1.ServerLivingEntityEvents;
 import net.fabricmc.fabric.api.event.player.UseItemCallback;
+import net.gooseman.inferno_utils.config.InfernoConfig;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Items;
+import net.minecraft.text.Text;
 import net.minecraft.util.ActionResult;
 import net.minecraft.world.GameMode;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.List;
 
 
 public class InfernoUtils implements ModInitializer {
@@ -24,12 +31,29 @@ public class InfernoUtils implements ModInitializer {
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
 
-		LOGGER.info("Hello Fabric world!");
+
+
 		UseItemCallback.EVENT.register((player, world, hand) -> {
 			if (!world.isClient && player.getGameMode() != GameMode.SPECTATOR && player.getStackInHand(hand).isOf(Items.FIREWORK_ROCKET)) {
 				return ActionResult.FAIL;
 			}
-			return ActionResult.SUCCESS;
+			return ActionResult.PASS;
         });
+
+		ServerLivingEntityEvents.AFTER_DEATH.register((entity, damageSource) -> {
+			if (entity instanceof PlayerEntity playerEntity) {
+				playerEntity.sendMessage(Text.of("booo hooo, let me play a sad song for you on the world's smallest violin"), false);
+				String deathTypeId = damageSource.getTypeRegistryEntry().getIdAsString();
+				Entity attacker = damageSource.getAttacker();
+				List<String> banExclusions = List.of(InfernoConfig.getStringArray("ban_exclusions"));
+				playerEntity.sendMessage(Text.of(String.format("Attacker is player:%b;Isn't excluded:%b;Attacker isn't victim:%b", attacker instanceof PlayerEntity, !banExclusions.contains(deathTypeId), attacker != entity)), false);
+				playerEntity.sendMessage(Text.of("DeathType id:" + deathTypeId), false);
+				if ((attacker instanceof PlayerEntity || !banExclusions.contains(deathTypeId)) && attacker != entity) {
+					playerEntity.sendMessage(Text.of("Death by player"), false);
+				} else {
+					playerEntity.sendMessage(Text.of("Git gud scrub"), false);
+				}
+			}
+		});
 	}
 }
---
+++ src/main/java/net/gooseman/inferno_utils/config/InfernoConfig.java
@@ -0,0 +1,23 @@
+package net.gooseman.inferno_utils.config;
+
+
+public class InfernoConfig {
+    public static String provider(String namespace) {
+        return """
+                    # Semi-Hardcore
+                    
+                    ban_time=8h # How long the death ban should last | Default=8h
+                    # What Death/Damage Types shouldn't cause a ban, comma seperated list
+                    # For possible values and their explanations check out https://minecraft.wiki/w/Damage_type#List_of_damage_types
+                    # Append minecraft: everytwhere
+                    ban_exclusions=[minecraft:sweet_berry_bush,minecraft:sting,starve,minecraft:spit,minecraft:sonic_boom,minecraft:outside_border,minecraft:out_of_world,minecraft:mob_projectile,minecraft:mob_attack_no_aggro,minecraft:mob_attack,minecraft:lightning_bolt,minecraft:hot_floor,minecraft:generic_kill,minecraft:generic,minecraft:freeze,minecraft:fly_into_wall,minecraft:fireball,minecraft:ender_pearl,minecraft:drown,minecraft:cramming,minecraft:campfire,minecraft:cactus,minecraft:arrow]
+                    """;
+    }
+    public static SimpleConfig config = SimpleConfig.of("inferno_utils-config").provider(InfernoConfig::provider).request();
+
+    public static String[] getStringArray(String key) {
+        String arrayString = config.getOrDefault(key, "[]");
+        arrayString = arrayString.substring(1, arrayString.length() - 1);
+        return arrayString.split(",");
+    }
+}
